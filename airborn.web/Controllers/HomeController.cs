f3cc2e7f5ae33ad1a058efd6b82bef8c5c51854f
alexjcampbell <alexjcampbell@gmail.com>
2021-04-17 21:48:22 +1200

    Text changes

--- airborn.web/Controllers/HomeController.cs
+++ airborn.web/Controllers/HomeController.cs
@@ -68,6 +68,11 @@ namespace Airborn.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
