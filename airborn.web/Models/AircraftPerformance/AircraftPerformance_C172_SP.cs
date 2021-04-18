8852ca443962b3db4f0135149db3ae208828f55f
alexjcampbell <alexjcampbell@gmail.com>
2021-04-19 11:16:12 +1200

    Updated takeoff and landing distance adjustments for C172 SP

--- airborn.web/Models/AircraftPerformance/AircraftPerformance_C172_SP.cs
+++ airborn.web/Models/AircraftPerformance/AircraftPerformance_C172_SP.cs
@@ -14,15 +14,13 @@ namespace Airborn.web.Models
 
             if (Scenario.HeadwindComponent > 0)
             {
-                // subtract 10% for each 12 knots of headwind
-                takeoffDistance = takeoffDistance * (1 - ((Scenario.HeadwindComponent / 12) * 0.1));
+                // subtract 10% for each 9 knots of headwind
+                takeoffDistance = takeoffDistance * (1 - ((Scenario.HeadwindComponent / 9) * 0.1));
             }
             else if (Scenario.HeadwindComponent < 0)
             {
                 // add 10% for each 2 knots of tailwind up to 10 knots
                 double adjustment = (Scenario.HeadwindComponent / 2) * 0.1;
-
-                if (adjustment > 0.5) { adjustment = 0.5; }
 
                 takeoffDistance = takeoffDistance * (1 - adjustment);
 
@@ -37,15 +35,13 @@ namespace Airborn.web.Models
         {
             if (Scenario.HeadwindComponent > 0)
             {
-                // subtract 10% for each 13 knots of headwind
-                landingDistance = landingDistance * (1 - ((Scenario.HeadwindComponent / 13) * 0.1));
+                // subtract 10% for each 9 knots of headwind
+                landingDistance = landingDistance * (1 - ((Scenario.HeadwindComponent / 9) * 0.1));
             }
             else if (Scenario.HeadwindComponent < 0)
             {
                 // add 10% for each 2 knots of tailwind up to 10 knots
                 double adjustment = (Scenario.HeadwindComponent / 2) * 0.1;
-
-                if (adjustment > 0.5) { adjustment = 0.5; }
 
                 landingDistance = landingDistance * (1 - adjustment);
 
