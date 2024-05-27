45b36f8c177b9d826f7dca730554581492893ff8
Alex Campbell <alexjcampbell@gmail.com>
2024-05-27 11:20:49 -0700

    Fixing migration

---
+++ airborn.web/Migrations/20240527182025_FixingAirports_fk_CountryId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace airborn.web.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingAirports_fk_CountryId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
