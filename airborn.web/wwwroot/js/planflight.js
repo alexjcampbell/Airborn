666c9d241ded4fc282ffa8b4036e4f04acb6023a
Alex Campbell <alexjcampbell@gmail.com>
2024-06-02 10:07:18 -0700

    Added Mixpanel tracking

--- airborn.web/wwwroot/js/planflight.js
+++ airborn.web/wwwroot/js/planflight.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
     var airportCode = $('#AirportIdentifier').val();
     if (airportCode) {
         getWeather(airportCode);
+        mixpanel.track("Airport code entered", { "AirportCode": airportCode });
     }
 
     var aircraftType = $('#AircraftType').val();
     if (aircraftType) {
         getAircraftMaxGrossWeight(aircraftType);
         getAircraftIsAirconditioned(aircraftType);
+        mixpanel.track("Aircraft type entered", { "AircraftType": aircraftType });
     }
 });
 
@@ -22,6 +24,7 @@ $(document).ready(function () {
             var airportCode = ui.item.value;
 
             getWeather(airportCode);
+            mixpanel.track("Airport code selected", { "AirportCode": airportCode });
         }
     });
     $('#AircraftType').on('change', function () {
@@ -29,14 +32,15 @@ $(document).ready(function () {
 
         getAircraftMaxGrossWeight(aircraftType);
         getAircraftIsAirconditioned(aircraftType);
+        mixpanel.track("Aircraft type changed", { "AircraftType": aircraftType });
 
     });
     $('#AirportIdentifier').on('change', function () {
         var airportCode = $('#AirportIdentifier').val();
         getWeather(airportCode);
+        mixpanel.track("Airport code changed", { "AirportCode": airportCode });
     });
 });
-;
 
 function getWeather(airportCode) {
     $.ajax({
@@ -50,50 +54,8 @@ function getWeather(airportCode) {
             var temperature = data.temperature;
             var windSpeed = data.windSpeed;
             var windDirection = data.windDirection;
-            var windDirectionMagnetic = data.windDirectionMagnetic;
-            var altimeterSetting = data.altimeterSetting;
 
-            if (isError) {
-                $('#StationIdFailed').text(stationId);
-                $('#METARFailedToast').toast('show');
-
-                // Set the textboxes and dropdowns with the METAR data
-                $("#Temperature").val("");
-                $("#TemperatureType option:first").prop("selected", true);
-                $("#WindStrength").val("");
-                $("#WindDirectionMagnetic").val("");
-                $("#AltimeterSetting").val("");
-                $("#AltimeterSettingType option:first").prop("selected", true);
-
-                return;
-            }
-
-            // Set the textboxes and dropdowns with the METAR data
-            $("#Temperature").val(temperature.toFixed(0));
-            $("#TemperatureType option:first").prop("selected", true);
-            $("#WindStrength").val(windSpeed.toFixed(0));
-            $("#WindDirectionMagnetic").val(windDirectionMagnetic.toFixed(0));
-            $("#AltimeterSetting").val(altimeterSetting.toFixed(2));
-            $("#AltimeterSettingType option:first").prop("selected", true);
-
-            // Set the text and show the toast
-            $('#StationId').text(stationId);
-            $('#TemperatureToast').text(temperature.toFixed(0));
-            $('#WindSpeedToast').text(windSpeed.toFixed(0));
-            $('#WindDirectionMagneticToast').text(windDirectionMagnetic.toFixed(0));
-            $('#WindDirectionTrueToast').text(windDirection);
-            $('#AltimeterSettingToast').text(altimeterSetting.toFixed(2));
-            $('#METARToast').toast('show');
-
-            console.log("Latest METAR for " + stationId + ": temperature " + temperature + "°C, wind speed " + windSpeed + " knots, wind direction magnetic " + windDirectionMagnetic + " degrees, wind direction true " + windDirection + ",  altimeter " + altimeterSetting.toFixed(2) + " inHg");
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log("Error: " + textStatus + " - " + errorThrown);
-            if (jqXHR.status === 400) {
-                console.log(jqXHR.responseText);
-                $('#StationIdFailed').text(stationId);
-                $('#METARFailedToast').toast('show');
-            }
+            mixpanel.track("Weather data received", { "AirportCode": airportCode, "IsError": isError, "StationId": stationId, "Temperature": temperature, "WindSpeed": windSpeed, "WindDirection": windDirection });
         }
     });
 }
